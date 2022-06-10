e01960f1ca64ff30341ca0f455d390a79ac20be7
Cristhian Sandoval <crissdavidsando@gmail.com>
2022-06-10 16:46:05 -0500

    feeat(src): + 90

--- src/main/java/co/edu/eafit/protobootapp/Calculadora.java
+++ src/main/java/co/edu/eafit/protobootapp/Calculadora.java
@@ -3,7 +3,7 @@ package co.edu.perficient.protobootapp;
 public class Calculadora {
 
     public Double cuadrado(Double numero) {
-        return numero * numero;
+        return numero * numero + 90;
     }
     
     public Double cubo(Double numero) {
